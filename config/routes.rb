b51fdc2d532f7ca2791bab7396e0a6d35b9b907a
Rajan Joshi <rajan.joshi@onthebeach.co.uk>
2020-02-23 15:30:22 +0000

    Add orders page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
+  get 'orders/index'
+
   resources :products
+  resources :orders, only:[:index]
   resources :order_items
   resource :carts do
     get :show
     post :order_now
   end
   match 'order_now' => 'carts#order_now', :via => :post
-  
   root "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
