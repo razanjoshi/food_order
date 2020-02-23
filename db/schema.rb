66853b4cf7434ff057e643f43251a66c11be31d0
Rajan Joshi <rajan.joshi@onthebeach.co.uk>
2020-02-23 19:12:58 +0000

    Add image to product.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200223161100) do
+ActiveRecord::Schema.define(version: 20200223184325) do
 
   create_table "order_items", force: :cascade do |t|
     t.integer  "product_id"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20200223161100) do
     t.float    "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "picture"
   end
 
   create_table "users", force: :cascade do |t|
