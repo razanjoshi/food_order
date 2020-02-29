ff87e951c7185b6a2f71ee4eb0ac2d4ace9d3841
Rajan Joshi <rajan.joshi@onthebeach.co.uk>
2020-02-29 22:13:10 +0000

    Test commit.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,6 +83,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.default_url_options = { :host => 'localhost' }
   config.action_mailer.smtp_settings = {
     :address              => "smtp.mailtrap.io",
     :port                 => 587,
